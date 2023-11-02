3452d5fcf1de020045397b0bcc3af79688699cae
HritikaBelekar <149709903+HritikaBelekar@users.noreply.github.com>
2023-11-03 01:13:21 +0530

    Add files via upload

--- app_fr.py
+++ app_fr.py
@@ -5,7 +5,7 @@ from PIL import Image
 from streamlit_option_menu import option_menu
 
 
-image = Image.open('/mount/src/Smart_Campus/Architechture_Face_Recog.png')
+image = Image.open('/mount/src/SmartCampus/Architechture_Face_Recog.png')
 
 with st.sidebar:
     
