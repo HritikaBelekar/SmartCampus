01cc175a5009afeeafe2a3d9ca94ffe50d8f3047
HritikaBelekar <149709903+HritikaBelekar@users.noreply.github.com>
2023-11-03 10:36:42 +0530

    Update app_fr.py

--- app_fr.py
+++ app_fr.py
@@ -6,7 +6,7 @@ from streamlit_option_menu import option_menu
 
 
 #archimage = Image.open('/mount/src/smartcampus/Architechture_Face_Recog.jpg')
-smartcampusimage = Image.open('/mount/src/smartcampus/SmartCampus.jpg')
+#smartcampusimage = Image.open('/mount/src/smartcampus/SmartCampus.jpg')
 
 with st.sidebar:
     
@@ -25,7 +25,7 @@ if (selected == 'About Project'):
     st.title('Smart Campus Surveillance & Guidance System')
     st.markdown('Aim of the project is to build a machine learning based Smart Campus Surveillance model which checks whether students are attending \
         the lectures or bunking the lectures based on the camera feed received from the camera installed in the campus. It will send a alert notification to respective HOD or Class Teacher about the bunks done by student')
-    st.image(smartcampusimage, caption='')
+    #st.image(smartcampusimage, caption='')
   
 
 if (selected == 'Project Contributors'):
